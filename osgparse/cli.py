eea863a61b2dd18e033adeabc65618f0fff6a99d
Zhe Zhang <zzxuanyuan@gmail.com>
2017-07-31 22:07:58 -0500

    Sorry, changed the authors :(

--- osgparse/cli.py
+++ osgparse/cli.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2016 Andi Albrecht, albrecht.andi@gmail.com
+# Copyright (C) 2017 Zhe Zhang, zzxuanyuan@gmail.com
 #
-# This module is part of python-sqlparse and is released under
+# This module is part of failure analysis of OSG project and is released under
 # the BSD License: https://opensource.org/licenses/BSD-3-Clause
 
 import argparse
